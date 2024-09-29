408ab2d9a88959347c516e401d1f71f68185ea5c
shetshay <and.ibrahim.rew@gmail.com>
2024-09-28 20:36:14 -0700

    center homepage

--- Website/app/HomePage/page.tsx
+++ Website/app/HomePage/page.tsx
@@ -1,4 +1,3 @@
-// app/HomePage/page.tsx
 "use client"; // Marks this as a client component
 import BackToHomeButton from "../components/BackToHomeButton";
 import HomeLayout from "../components/HomeLayout"; // Import the new HomeLayout component
@@ -8,28 +7,22 @@ export default function HomePage() {
   return (
     <HomeLayout>
       {/* Unique content of the HomePage */}
-      <div className="flex flex-col items-center p-6 text-center">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Our Home Page</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen p-6 text-center">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Facial Rec</h1>
         <p className="text-lg mb-6">
-          Explore our platform to learn more about us and our features.
+          A cloud-based storage system using facial recognition.
         </p>
 
         {/* Navigation Links to Subpages */}
         <div className="flex gap-4">
           <Link
-            href="/HomePage/services"
-            className="px-4 py-2 bg-blue-500 rounded-lg text-white hover:bg-blue-600"
-          >
-            Our Services
-          </Link>
-          <Link
-            href="/HomePage/signUp"
+            href="/user"
             className="px-4 py-2 bg-blue-500 rounded-lg text-white hover:bg-blue-600"
           >
             Sign Up
           </Link>
           <Link
-            href="/HomePage/signIn"
+            href="/user"
             className="px-4 py-2 bg-blue-500 rounded-lg text-white hover:bg-blue-600"
           >
             Sign In
